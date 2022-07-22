a11a8b1ef96771a79bf65c8cfa4e58e874a64987
Rodrigo <51071212+rodrigowc@users.noreply.github.com>
2022-07-22 17:14:33 -0300

    Update nav-bar.php

--- views/outhers/nav-bar.php
+++ views/outhers/nav-bar.php
@@ -1,10 +1,8 @@
-<nav>
+<nav class="blue">
   <div class="nav-wrapper">
-    <a href="#" class="brand-logo">Logo</a>
+    <a href="#" class="brand-logo">Amenix FrameWork</a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li><a href="sass.html">Sass</a></li>
-      <li><a href="badges.html">Components</a></li>
-      <li><a href="collapsible.html">JavaScript</a></li>
+      <li><a href="https://github.com/rodrigowc/amenix-framework" target="_blank">GitHub</a></li>
     </ul>
   </div>
 </nav>
